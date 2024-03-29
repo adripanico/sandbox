da6217e51ec4f0ab7361b82fa303ecebb7a70a2b
adripanico <adripanicodev@gmail.com>
2024-03-29 20:57:07 +0100

    Update brush pattern

--- src/constants.ts
+++ src/constants.ts
@@ -1,16 +1,13 @@
 export const CANVAS_SIZE = 200;
+
 export const ROW_SIZE = CANVAS_SIZE * 4; // 4 numbers (r, g, b, a) per pixel
-// export const BRUSH_PATTERN = [
-//     [0, 0, 0.5, 0, 0],
-//     [0, 0.5, 1, 0.5, 0],
-//     [0.5, 1, 1, 1, 0.5],
-//     [0, 0.5, 1, 0.5, 0],
-//     [0, 0, 0.5, 0, 0],
-// ];
+
+// TODO make the patter editable
+// represents the pixels to be drawn where 0-1 is the opacity of each pixel
 export const BRUSH_PATTERN = [
-  [0, 0, 0.5, 0, 0],
-  [0, 0.5, 1, 0.5, 0],
+  [0, 0, 0.8, 0, 0],
+  [0, 0.8, 1, 0.5, 0],
+  [0.8, 1, 1, 1, 0.8],
   [0.5, 1, 1, 1, 0.5],
-  [0.5, 1, 1, 1, 0.5],
-  [0, 0.5, 0.5, 0.5, 0]
+  [0, 0.5, 0.8, 0.5, 0],
 ];
